e4a7a7c569286d90a563524edaaf1f28c93cb82d
lijunchang <lijunchang@itixiao.com>
2024-04-09 10:54:00 +0800

    chore: build

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,29 @@
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
 
 export default defineConfig({
+  server: {
+    host: true,
+    open: true,
+  },
   build: {
     target: 'es2015',
     lib: {
-      entry: './lib/index.ts',
+      entry: resolve(__dirname, 'lib/index.ts'),
       name: 'Dom2svg',
       fileName: 'dom2svg',
     },
+    rollupOptions: {
+      external: ['gradient-parser', 'postcss', 'postcss-value-parser'],
+      output: {
+        globals: {
+          'gradient-parser': 'gradientParser',
+          'postcss': 'postcss',
+          'postcss-value-parser': 'postcssValueParser',
+        },
+      },
+    },
   },
-  plugins: [dts({
-    entryRoot: 'lib',
-    outDir: 'dist/types',
-    staticImport: true,
-    insertTypesEntry: true,
-  })],
+  plugins: [dts({ rollupTypes: true })],
 })
